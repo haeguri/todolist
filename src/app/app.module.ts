ec435626c410acfbfb6a46294df9b91e1c505662
pacebook <haegyun821@gmail.com>
2017-09-03 22:14:14 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule
